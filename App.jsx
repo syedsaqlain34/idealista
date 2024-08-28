2b43c17b69af4913cd345148b7ac17f598b718ad
syedsaqlain34 <160273740+syedsaqlain34@users.noreply.github.com>
2024-08-28 19:10:51 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card1 from './components/Card1';
+import Card2 from './components/Card2';
+
+function App() {
+  return (
+    <div>
+      <Card1 />
+      <Card2 />
+    </div>
+  );
+}
+
+export default App;
